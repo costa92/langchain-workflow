13a61f39ba4a5cce31d3ccab4790ef9d15d41862
costa92 <944310536@qq.com>
2025-05-08 23:18:18 +0800

    feat(chattts): 新增语音合成测试功能并添加依赖
    在测试文件中实现完整的语音合成流程，新增 torchaudio 等依赖库支持

--- tests/test_chattts_demo.py
+++ tests/test_chattts_demo.py
@@ -1,14 +1,23 @@
 
 
-import torch
 import ChatTTS
-from IPython.display import Audio
+import torch
+import torchaudio
 
-# chat = ChatTTS.Chat()
 chat = ChatTTS.Chat()
+chat.load(compile=False) # Set to True for better performance
 
-chat.download_models(source='local')
+texts = ["用中英文混杂的方式，简单介绍宁波"]
 
-text = "用中英文混杂的方式，简单介绍宁波"
-wav = chat.infer(text)
-Audio(wav[0], rate=24_000, autoplay=True)
+wavs = chat.infer(texts)
+
+print(wavs)
+# # 转换 numpy → tensor，并添加 batch 维度 [1, samples]
+waveform = torch.from_numpy(wavs[0]).unsqueeze(0).float()
+# print(waveform)
+# # 保存为 WAV（明确指定 format）
+torchaudio.save("output1.wav", waveform, sample_rate=24000, format="wav")
+
+# wavs = chat.infer(texts)
+
+# torchaudio.save("output1.wav", torch.from_numpy(wavs[0]), 24000)
